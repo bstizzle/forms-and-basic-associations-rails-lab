2a03ad85408144485fa08e12861d8bac70071069
bstizzle <whereyouben@gmail.com>
2020-11-20 15:30:32 -0500

    built new form  with nested models in erb

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,37 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(n)
+    self.artist = Artist.find_or_create_by(name: n)
+  end 
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    end 
+  end
+
+  def genre_name=(n)
+    self.genre = Genre.find_or_create_by(name: n)
+  end 
+
+  def genre_name
+    if self.genre
+      self.genre.name
+    end 
+  end
+
+  def note_contents=(contents)
+    contents.each do |cont|
+      if cont != ""
+        self.notes.build(content: cont)
+      end
+    end
+  end 
+
+  def note_contents
+    self.notes.map{ |note| note.content}
+  end
 end
